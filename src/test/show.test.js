27e650c585faeb661002434a4c312cb6c254029e
Tomas Tulka <tomas.tulka@gmail.com>
2020-07-26 09:04:02 +0200

    toDocument

--- src/test/show.test.js
+++ src/test/show.test.js
@@ -4,7 +4,7 @@ import ShowPage from '../show.js';
 
 describe('ShowPage', () => {
 
-    const PAGE1 = new DOMParser().parseFromString(`
+    const PAGE1 = `
         <html>
             <head>
                 <title>TestPage1</title>
@@ -12,8 +12,8 @@ describe('ShowPage', () => {
                 <meta name="page1" content="page1">
             </head>
             <body>Page1</body>
-        </html>`, 'text/html');
-    const PAGE2 = new DOMParser().parseFromString(`
+        </html>`;
+    const PAGE2 = `
         <html>
             <head>
                 <title>TestPage2</title>
@@ -21,31 +21,37 @@ describe('ShowPage', () => {
                 <meta name="page2" content="page2">
             </head>
             <body>Page2</body>
-        </html>`, 'text/html');
+        </html>`;
     
     describe('show', () => {
         
         it('body is showned', () => { 
-            const doc = PAGE1.cloneNode(true);
-            const showPage = new ShowPage(doc);
+            const page1 = toDocument(PAGE1);
+            const page2 = toDocument(PAGE2);
+            const showPage = new ShowPage(page1);
 
-            showPage.show(PAGE2.cloneNode(true));
+            showPage.show(page2);
 
-            assert.equal(doc.body.innerText, PAGE2.body.innerText);
+            assert.equal(page1.body.innerHTML, page2.body.innerHTML);
         });
 
         it('headers are merged', () => {
-            const doc = PAGE1.cloneNode(true);
-            const showPage = new ShowPage(doc);
+            const page1 = toDocument(PAGE1);
+            const page2 = toDocument(PAGE2);
+            const showPage = new ShowPage(page1);
 
-            showPage.show(PAGE2.cloneNode(true));
+            showPage.show(page2);
 
-            const headers = Array.from(doc.head.children); 
+            const headers = Array.from(page1.head.children); 
 
-            assert.equal(doc.head.childElementCount, 3);
-            assert.equal(doc.head.querySelector('title').innerText, 'TestPage2');
-            assert.isTrue(!!doc.head.querySelector('meta[charset=utf-8]'));
-            assert.isTrue(!!doc.head.querySelector('meta[name=page2][content=page2]'));
+            assert.equal(page1.head.childElementCount, 3);
+            assert.equal(page1.head.querySelector('title').innerText, 'TestPage2');
+            assert.isTrue(!!page1.head.querySelector('meta[charset=utf-8]'));
+            assert.isTrue(!!page1.head.querySelector('meta[name=page2][content=page2]'));
         });
     });
+
+    function toDocument(html) {
+        return new DOMParser().parseFromString(html, 'text/html');
+    }
 });
